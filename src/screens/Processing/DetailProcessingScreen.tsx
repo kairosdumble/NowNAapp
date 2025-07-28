1ccf6adbaba08cd9e3a48d5293252efc876bdf09
aryeong <=>
2025-07-28 15:20:55 +0900

    - 접수되었습니다의 전체보기를 ReceivedScreen과 연결
    - 처리중입니다 전체보기를ProcessingScreen과 연결
    
    - 검색창(SearchScreen)에서 이전 창으로 돌아갈수 있도록 추가

---
+++ src/screens/Processing/DetailProcessingScreen.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import style from '../../style/DetailProcessingScreen';
+
+export default function DetailReceivedScreen() {
+  return (
+      <ScrollView>
+        {/*제목*/}
+        <View>
+
+        </View>
+
+        {/*심사진행단계*/}
+        <View>
+        </View>
+
+        {/*접수 내용*/}
+        <View>
+        </View>
+        {/*제안이유 및 주요 내용 */}
+
+        {/*구분선*/}
+
+
+      <ScrollView>
+      );
+}
+
