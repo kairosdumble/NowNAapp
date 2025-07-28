22dfb1381fad7502c3066b8cf2ab6f0666a5bd82
aryeong <=>
2025-07-28 15:21:15 +0900

    .

---
+++ src/screens/Received/DetailReceivedScreen.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, ScrollView,TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'; // 아이콘 import
+import styles from '../../styles/DetailReceivedScreen';
+
+
+export default function DetailReceivedScreen() {
+  return (
+      <ScrollView>
+        {/*제목*/}
+        <View>
+        
+        </View>
+        
+        {/*심사진행단계*/}
+        <View>
+        </View>
+        
+        {/*접수 내용*/}
+        <View>
+        </View>
+        {/*제안이유 및 주요 내용 */}
+        
+        {/*구분선*/}
+        
+      
+      <ScrollView>
+      );
+}
+
---
+++ src/screens/Received/ReceivedListScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import style from '../../styles/ListScreenStyles';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default function ReceivedListScreen() {
+  return (
+      <View>
+      </View>
+  );
+}
