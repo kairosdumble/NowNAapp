c4d1fbcbd7c6251db7391f14115bf3011f88cf64
aryeong <=>
2025-07-29 01:08:43 +0900

    설정 화면 구현(세부정보는 아직 추가 안함)

---
+++ src/screens/SettingScreen.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, TouchableOpacity,StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+
+export default function SearchScreen() {
+    return (
+        <View style ={styles.container}>
+            <View style={styles.separator} />
+
+            <TouchableOpacity >
+                <Text style={styles.font}>알림 설정</Text>
+            </TouchableOpacity>
+            <View style={styles.separator} />
+
+            <TouchableOpacity>
+                <Text style={styles.font}>글자 크기 설정</Text>
+            </TouchableOpacity>
+            <View style={styles.separator} />
+
+            <TouchableOpacity >
+                <Text style={styles.font}>이용약관/개인정보처리방침</Text>
+            </TouchableOpacity>
+            <View style={styles.separator} />
+
+            <Text style={styles.font}>앱 버전 확인</Text>
+            <View style={styles.separator} />
+
+        </View>
+        );
+    }
+
+const styles = StyleSheet.create({
+    container:{
+        paddingTop:70,
+        background:'#fff'
+        },
+    font:{
+        padding:10,
+        paddingLeft:30,
+        fontSize: 18,
+        fontWeight:'500',
+        },
+    separator: {
+        height: 1,
+        backgroundColor: '#ccc',
+        marginVertical: 5,
+      },
+    })
